5a5ae278218a8794dc89db209529972f0e0a203d
Artur <artur.prado@gmail.com>
2020-04-25 21:48:43 -0300

    Include header and reset default css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './main.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Olá mundo</h1>
     </div>
   );
