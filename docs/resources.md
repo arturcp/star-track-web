9587e40f991d014305a0d6ed03e40a48174ce84d
Artur <artur.prado@gmail.com>
2020-04-26 18:47:02 -0300

    Add baraja-js and show character cards

--- docs/resources.md
+++ docs/resources.md
@@ -15,3 +15,14 @@ This package provides functions to deal with routes.
 ```
 yarn add react-router-dom
 ```
+
+## baraja-js
+
+This package animates cards to simulate a shuffle effect.
+
+[Github](https://github.com/nuxy/baraja-js)
+[Demo](https://nuxy.github.io/baraja-js/)
+
+## Free image host
+
+https://freeimage.host/?lang=pt
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import api from '../../services/api';
 import './styles.css';
 
+import Baraja from 'baraja-js';
+import 'baraja-js/dist/baraja.css';
+
+// const baraja = new Baraja(container, options);
+
 export default class Main extends Component {
   state = {
     characters: [],
@@ -12,27 +17,46 @@ export default class Main extends Component {
     this.loadCharacters();
   }
 
+  loadCards = () => {
+    let container = document.querySelector('.baraja-container'),
+        options = {};
+
+    const baraja = new Baraja(container);
+    this.fanRight(baraja);
+  };
+
   loadCharacters = async () => {
     const response = await api.get('/characters');
     this.setState({ characters: response.data.characters });
-  }
+    this.loadCards();
+  };
+
+  fanRight = (baraja) => {
+    baraja.fan({
+      direction: 'right',
+      easing: 'ease-out',
+      origin: { x : 25, y : 100 },
+      speed: 500,
+      range: 90,
+      center: true
+    });
+  };
 
   render() {
     const { characters } = this.state;
 
     return (
-      <div>
-        <div className="characters-list">
+      <div className="baraja-cards">
+        <ul className="baraja-container">
           {characters.map(character => (
-            <article key={character.id}>
-              <h2>{character.name}</h2>
+            <li key={character.id}>
+              <img src={character.image_url} alt={character.name}/>
+              <h4>{character.name}</h4>
 
-              <div className="image-container">
-                <img src={character.image_url} alt={character.name}/>
-              </div>
-            </article>
+              <p>{character.biography}</p>
+            </li>
           ))}
-        </div>
+        </ul>
 
         <Link to="/about">Sobre</Link>
       </div>
