eb4776487bf58b192b894ece08a366af1e9be1a9
Artur <artur.prado@gmail.com>
2020-04-26 23:01:30 -0300

    Use heroku API

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api  = axios.create({ baseURL: 'http://localhost:4000' });
+const api  = axios.create({ baseURL: 'https://star-track-api.herokuapp.com' });
 
 export default api;
